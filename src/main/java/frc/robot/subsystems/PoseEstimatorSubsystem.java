5c69cdcc6a97524cd8ee3eb18ccbf0028c1d3f2d
allderdicerobotics <35245530+allderdicerobotics@users.noreply.github.com>
2023-03-22 20:01:46 -0400

    commit for pose stuff

--- src/main/java/frc/robot/subsystems/PoseEstimatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/PoseEstimatorSubsystem.java
@@ -169,7 +169,7 @@ public class PoseEstimatorSubsystem extends SubsystemBase {
     public void resetFieldPosition() {
         setCurrentPose(new Pose2d());
     }
-    
+
     // public Trajectory getScorePath(){
     //     // eventually pass in field-relative coords of the score stations
     //     // desired location (field-relative) x: 14.5, 1.45, 0.462
