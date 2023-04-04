9b38eb064bc49355815801909f982da24d36df6b
allderdicerobotics <35245530+allderdicerobotics@users.noreply.github.com>
2023-04-04 15:50:05 -0400

    comitttttttt

---
+++ src/main/java/frc/robot/commands/CSBalance.java
@@ -0,0 +1,10 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.*;
+import frc.robot.subsystems.DriveSubsystem;
+
+public class CSBalance extends CommandBase {
+    public CSBalance(DriveSubsystem drive) {
+        
+    }
+}
